b40eda4dff718d356b29b6ecd9c08cb542b16a36
Kiligmzq6 <130635742+Kiligmzq6@users.noreply.github.com>
2023-05-01 23:46:27 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name='chatgpt',
-    version='0.1',
-    packages=find_packages(),
+    name='GptCode',  # 对外模块的名字
+    version='1.0.2',  # 版本号
+    description='测试本地发布模块',  # 描述
+    requires=['openai'], # 定义依赖哪些模块
+    author='mazhiqiang',  # 作者
+    author_email='mazhiqiang2023@163.com',
+    py_modules=['GptCode.gpt'],  # 要发布的模块
 )
