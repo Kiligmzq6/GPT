2e2ee62c262fec22588f2faabe8101fafcd27607
Kiligmzq6 <130635742+Kiligmzq6@users.noreply.github.com>
2023-05-02 16:34:04 +0800

    gpt.py

--- gpt.py
+++ gpt.py
@@ -1,4 +1,8 @@
 import openai
+from PIL import Image
+from io import BytesIO
+import requests
+from IPython.display import display
 
 class Chat:
     def __init__(self, api_key, system_setting=None, language="中文", model="gpt-3.5-turbo") -> None:
@@ -39,11 +43,50 @@ class Chat:
         prompt = prompt + tail_message
         return prompt
 
+    def translate(self,prompt,aim_language = "英语"):
+        """
+        :param prompt: 原始语言
+        :param aim_language: 要翻译成的语种
+        :return: 翻译结果
+        """
+        trans = f"请把以下语言翻译为{aim_language}。"
+        response = self.get_answer(messages=self.system_setting_list + [{'role': 'user', 'content': trans+prompt}])
+        return response
+
+    def extract_picture_prompt(self,prompt):
+        extract = "以下句子描述了一张图片，请从中提取出对图片的描述，或者想象一下图片的描述，描述请尽量详细。"
+        response = self.get_answer(messages=self.system_setting_list + [{'role': 'user', 'content': extract + prompt}])
+        return response
+
     def get_answer(self, messages):
         response = openai.ChatCompletion.create(model=self.model, messages=messages)
         answer = response.choices[0].message['content']
         return answer
 
+    def painting(self,prompt,image_size = 512,return_img = False):
+        """
+        :param prompt: 图像描述
+        :param image_size: 图像大小 image_size*image_size
+        :param return_img: 是否返回图像，若不返回则直接展示
+        :return: 图像
+        """
+        prompt = self.extract_picture_prompt(prompt)
+        prompt = self.translate(prompt)
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size=str(image_size)+"x"+str(image_size)
+        )
+        url = response['data'][0]['url']
+        # 获取图片链接的二进制数据
+        response = requests.get(url)
+        image_data = response.content
+
+        # 将二进制数据转换成图片对象并展示
+        image = Image.open(BytesIO(image_data))
+        if return_img:
+            return response['data'][0]['url']
+        display(image)
     # 提示chatgpt
     def ask(self, prompt,reference=True, use_history=False, history_number=5):
         """
@@ -54,7 +97,7 @@ class Chat:
         :return: 此次问题的回复
         """
         if reference:
-            prompt = self.generate_prompt(prompt)
+            prompt = self.generate_pxrompt(prompt)
         self.conversation_list.append({"role": "user", "content": prompt})
         if use_history:
             # 基于历史history_number条提问记录和当前问题进行回答,虽然会结合上下文，但是会比较耗token
